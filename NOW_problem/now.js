16347963e893f1dedf6d47db6260f28b6d469b2e
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-08-21 21:42:50 +0530

    Now problems

--- NOW_problem/now.js
+++ NOW_problem/now.js
@@ -5,9 +5,10 @@ Description
 Sandhya is running up a staircase with N steps, and can hop(jump) either 1 step, 2 steps or 3 steps at a time. You have to count, how many possible ways Sandhya can run up to the stairs.
 
 Input
+
 input Format
 
-Input contains integer N that is number of steps
+Input contains integer N that is number of steps.
 
 Constraints
 
